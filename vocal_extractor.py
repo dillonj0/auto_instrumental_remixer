7aa85ed52cf2be71a1c8e0b40d2ddb6b31696e48
Dillon Jensen <dillonj0@stanford.edu>
2026-01-19 14:01:51 -0800

    first attempt at isolating lyrics

--- vocal_extractor.py
+++ vocal_extractor.py
@@ -1,11 +1,24 @@
 import soundfile as sf
 import numpy as np
 
+print('Importing audio...')
 lyr, lyrr = sf.read('audio/ODESZA_say_my_name.wav')
 ins, insr = sf.read('audio/ODESZA_say_my_name_instrumental.wav')
 
-print('lyrical sample rate:', lyrr)
-print('lyrical length:', len(lyr))
+print('\tlyrical sample rate:', lyrr)
+print('\tlyrical length:', len(lyr))
 
-print('instrumental sample rate:', insr)
-print('instrumental length:', len(ins))
+print('\tinstrumental sample rate:', insr)
+print('\tinstrumental length:', len(ins))
+
+print('Performing DFT...')
+Lyr = np.fft.fft(lyr)
+Ins = np.fft.fft(ins)
+
+print('Isolating lyrics...')
+L_only = Lyr - Ins # Only the lyrics should remain
+l_only = np.fft.ifft(L_only)
+l_only_real = np.real(l_only) # .wav has to be real-valued
+
+print('Writing lyrics-only file...')
+sf.write('audio/lyrics_only.wav', l_only_real, lyrr)
